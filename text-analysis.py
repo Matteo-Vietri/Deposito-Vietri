9e05d8a3f687be6ce9d176abfcd82e55e39c23ed
Matteo-Vietri <matteo.vietri@it.ey.com>
2025-08-20 11:24:47 +0200

    Update text-analysis.py
    feat(text_analysis): add top 5 words function

--- text-analysis.py
+++ text-analysis.py
@@ -13,6 +13,23 @@ def conta_parole(text):
     word_count = len(text.split())
     return word_count
 
+def parole_frequenti(text):
+    for ch in "-.,\n\r!?;:":
+        text = text.replace(ch, ' ')
+    text = text.lower()
+    words = text.split()
+    unique_words = {}
+    for word in words:
+        if word in unique_words:
+            unique_words[word] = unique_words[word] + 1
+        else:
+            unique_words[word] = 1
+
+    sorted_items = sorted(unique_words.items(), key=lambda item: item[1], reverse=True)
+    return sorted_items[:5]
+
+
 
 print(conta_righe(content))
 print(conta_parole(content))
+print(parole_frequenti(content))
